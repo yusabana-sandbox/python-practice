e70feb9510a249f4f841da0b3ec5c70f677dae78
Yuji Takaesu <taka.esu.user@gmail.com>
2017-06-02 09:38:07 +0900

    add str sample

---
+++ str.py
@@ -0,0 +1,34 @@
+# vim: fileencoding=utf-8
+
+a = """bbbbbb
+aaaaaaaaaa
+"""
+
+b = '''aaaaa
+dfdfd3'''
+
+
+print(a)
+print('--------------')
+print(b)
+
+
+print('=================')
+
+print(str(1))
+print(int('1'))
+
+print('=================')
+
+print('3' in b)
+
+print('=================')
+
+m1 = 'I am {}'.format('Yujiii')
+print(m1)
+
+m2 = '{2} {0} {1}'.format('aa', 'bb', 'cc')
+print(m2)
+
+m3 = '{aa} AND {bb}'.format(bb='なんだって', aa=1)
+print(m3)
