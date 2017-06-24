ab67725a245e0f83cf137ea683754302518f739a
Yuji Takaesu <taka.esu.user@gmail.com>
2017-06-24 23:23:57 +0900

    update math packages to numeric.py

--- numeric.py
+++ numeric.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+import math
 
 print(0b11)
 print(0o17)
@@ -7,3 +7,12 @@ print(0xb)
 # int('2.1') # Error
 
 print(type(0xb))
+
+print(2**1000)
+print(1.00000000001 ** 100000000000)
+
+print((1+1j) * (1-1j))
+
+
+print(math.pi)
+print(math.sin(math.pi/2))
