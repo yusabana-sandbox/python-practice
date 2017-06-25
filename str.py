69cf2409e75e3c2a6a0ff419c4036e978f6a5236
Yuji Takaesu <taka.esu.user@gmail.com>
2017-06-25 20:03:55 +0900

    add usage string

--- str.py
+++ str.py
@@ -42,6 +42,19 @@ print('=' * 20)
 aa = ['1', '2', 'bb']
 print(','.join(aa))
 # リストやタプルを文字列展開する場合
+print('list is %s' % ['aaaa', 1, 2])
 # http://qiita.com/tomotaka_ito/items/594ee1396cf982ba9887
 print('divmod: %s' % (divmod(5, 2),))
 
+
+print('aaa is %(a)s' % dict(a='AAA'))
+
+
+str = 'abcdefghijklmnopqrstuvwxyz'
+print(len(str))
+print(str[:23])
+print(str[0:])
+print(str[:])
+print(str[::2])  # 添字が偶数番目の取り出し(先頭から一つ飛ばし)
+print(str[1::2]) # 添字書き数番目の取り出し
+print(str[::-1]) # 逆順取り出し
