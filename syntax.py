d944c0ed557ce901b14a80d16778dea0b64fae8b
Yuji Takaesu <taka.esu.user@gmail.com>
2017-06-26 05:17:27 +0900

    add for, while syntax to syntax.py

--- syntax.py
+++ syntax.py
@@ -3,15 +3,47 @@
 
 def main():
     print('## for ##')
-    list = ['aa', 'bb', 'cc']
-    for data in list:
-        print(data)
+    lis = ['aa', 'bb', 'cc']
+    for data in lis:
+        print('data=', data)
+
+    print('*' * 40)
 
     for i in range(10):
         print(i)
 
-    for i, data in enumerate(list):
+    print('*' * 40)
+
+    for i in range(1, 10, 2):
+        print(i)
+    else:
+        print('終了')
+
+    print('*' * 40)
+
+    for i, data in enumerate(lis):
         print(i, data)
+
+    print('*' * 40)
+
+    numbers = [ x**2 for x in range(10) ]
+    print(numbers)
+
+
+    print('## while ##')
+
+    n = 0
+    while n < 10:
+        if n == 2:
+            n += 2
+            continue
+
+        print(n)
+        n += 2
+    else:
+        print('終了')
+
+
 
 
 if __name__ == '__main__':
