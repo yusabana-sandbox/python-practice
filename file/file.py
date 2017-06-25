68759d7ba7364ea2667ba7e1861dea51e1499138
Yuji Takaesu <taka.esu.user@gmail.com>
2017-06-26 06:05:28 +0900

    add readline() of file io

--- file/file.py
+++ file/file.py
@@ -2,12 +2,25 @@
 
 def main():
     f = open('spam.txt', 'w', encoding='utf-8')
-    f.write('スパム、ハム')
+    f.write('スパム、ハム\n')
+    f.write('hoge,fuga')
     f.close()
 
     read_file = open('spam.txt', encoding='utf-8')
     print(read_file.read())
     read_file.close
+
+    print('*' * 30)
+
+    read_file2 = open('spam.txt', encoding='utf-8')
+    while True:
+        s = read_file2.readline().rstrip() # readlineが改行コードも含めて取得するので余計な改行コードが含まれる
+        if s:
+            print(s)
+        else:
+            break
+
+    print('*' * 30)
 
     # withを使うとブロックを終了すると自動で閉じる
     with open('spam.txt', encoding='utf-8') as f:
