1698bf4979a1e09fe166ac36507c363f95564eeb
Yuji Takaesu <taka.esu.user@gmail.com>
2017-11-29 17:20:38 +0900

    autopep8実行してフォーマット

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,13 @@
 # vim: fileencoding=utf-8
 
+
 def main():
     for num in range(1, 101):
         if num % 15 == 0:
             print('FizzBuzz')
         elif num % 3 == 0:
             print('Fizz')
-        elif num % 5 ==0:
+        elif num % 5 == 0:
             print('Buzz')
         else:
             print(num)
@@ -14,4 +15,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
